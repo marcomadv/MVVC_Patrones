20f049c5d3d3a46ff71482c4e475c4af0727895b
marcomadv <imarcoma@gmail.com>
2023-10-04 21:37:13 +0200

    Primer Commit

---
+++ MVVMPatrones/UI/Splash/Controller/SplashViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SplashViewController.swift
+//  MVVMPatrones
+//
+//  Created by Marco Muñoz on 4/10/23.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    @IBOutlet weak var activitiIndicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ MVVMPatrones/UI/Splash/ViewModel/SplashViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SplashViewModel.swift
+//  MVVMPatrones
+//
+//  Created by Marco Muñoz on 4/10/23.
+//
+
+import Foundation
